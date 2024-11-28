a5d719f6e8646083ae2afe4282a43afcc7b4000e
Kidhar Bachan <kidharb@gmail.com>
2024-11-28 20:21:34 +0200

    Initial

---
+++ budgetapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import PDFContent
+
+admin.site.register(PDFContent)
---
+++ budgetapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from PyPDF2 import PdfReader
+import io
+from .models import PDFContent
+from .forms import PDFUploadForm
+
+def upload_pdf(request):
+    if request.method == 'POST':
+        form = PDFUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            pdf_file = request.FILES['pdf']
+            pdf_reader = PdfReader(io.BytesIO(pdf_file.read()))
+            text = ''
+            for page_number in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[page_number]
+                text += page.extract_text()
+
+            # Save parsed content to the database
+            pdf_content = PDFContent(title=pdf_file.name, content=text)
+            pdf_content.save()
+            return redirect('view_pdfs')
+
+    else:
+        form = PDFUploadForm()
+
+    return render(request, 'upload.html', {'form': form})
+
+def view_pdfs(request):
+    pdfs = PDFContent.objects.all()
+    return render(request, 'view_pdfs.html', {'pdfs': pdfs})
